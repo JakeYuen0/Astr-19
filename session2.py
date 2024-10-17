4b4a81002e893f573751141a87d1bab88abda523
JakeYuen0 <jaketyuen@gmail.com>
2024-10-17 14:49:09 -0700

    Add files via upload

---
+++ session2.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+x = 2.3
+y = 1.2
+
+print("f1 = ", 2.3)
+print("f2 = ", 1.2)
+
+print("Float sum")
+
+print("f1 + f2 = ", x + y)
+
+x = 5
+y = 4
+
+print("int1 = ",5)
+print("int2 = ",4)
+
+print("int difference")
+
+print("int1 - int2 = ", x - y)
+
+x = 2.3
+y = 4
+
+print("f1 = ",2.3)
+print("int2 = ",4)
+
+print("f1 * int2 =", x * y)
