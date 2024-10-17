f4cd04665219b9bf04cd7fd64934e282129918ae
JakeYuen0 <jaketyuen@gmail.com>
2024-10-17 15:09:43 -0700

    Add files via upload

---
+++ session-prompt-3.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def function(x):
+    return x ** 3 + 8
+def main():
+    result = function(4)
+    if result > 27:
+        print("YAY")
+if __name__ == "__main__":
+    main()
+
+
