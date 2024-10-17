7e3bc7be7d0dd7b291cf5fdc4f0616e40d59c481
JakeYuen0 <jaketyuen@gmail.com>
2024-10-17 13:44:46 -0700

    Add files via upload

---
+++ session-prompt-1.py
@@ -0,0 +1,9 @@
+def PrintPrompt():
+    print("My name is Jake Yuen. My pronouns are He/him. I love the movie Ready Player One. It has amazing CGI. I love tacos.")
+    
+    
+def main():
+    PrintPrompt()
+    
+if __name__=="__main__":
+    main()
